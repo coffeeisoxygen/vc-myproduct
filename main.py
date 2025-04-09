a5432b8cddfc7a908eb03e681428f2b7cd9ca841
Hasan Maki <42813480+nekadcoding@users.noreply.github.com>
2025-04-09 18:12:37 +0700

    Implement logging in main function for enhanced message tracking

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+from app.utils.logger import get_logger
+
+# Dapatkan objek logger
+logger = get_logger("main")
+
+
 def main():
     print("Hello from vc-myproduct!")
+    # Gunakan logger untuk mencatat pesan
+    logger.info("Hello from vc-myproduct!")
+    logger.debug("Ini pesan debug")
+    logger.warning("Ini pesan peringatan")
+    logger.error("Ini pesan error")
+    logger.critical("Ini pesan kritis")
 
 
 if __name__ == "__main__":
